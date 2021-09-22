32fd9c6dc1fc6a45781329bdac81fc6f0052c5c1
ghayani GN <91198271+Ghayani1098@users.noreply.github.com>
2021-09-22 18:24:20 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_flutter/utils/text.dart';
+import 'package:movie_app_flutter/widgets/toprated.dart';
+import 'package:movie_app_flutter/widgets/trending.dart';
+import 'package:tmdb_api/tmdb_api.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      theme: ThemeData(
+          brightness: Brightness.dark, primaryColor: Colors.green),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final String apikey = '8c03c46fe6db2658c8cb287fb8d62195';
+  final String readaccesstoken ='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzAzYzQ2ZmU2ZGIyNjU4YzhjYjI4N2ZiOGQ2MjE5NSIsInN1YiI6IjYxNDllNjNiMTY4OTM1MDA4ZjBmZTU0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4JGgFe-6uahjb3h3ak8sOkPoRge2NX94uKywf3OERrY';
+  List trendingmovies = [];
+  List topratedmovies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadmovies();
+  }
+
+  loadmovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(
+      ApiKeys(apikey, readaccesstoken),
+      logConfig: ConfigLogger(
+        showLogs: true,
+        showErrorLogs: true,
+      ),
+    );
+
+    Map trendingresult = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map topratedresult = await tmdbWithCustomLogs.v3.movies.getTopRated();
+    print((trendingresult));
+    setState(() {
+      trendingmovies = trendingresult['results'];
+      topratedmovies = topratedresult['results'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: modified_text(text: 'Flutter Movie App ❤️'),
+          backgroundColor: Colors.transparent,
+        ),
+        body: ListView(
+          children: [
+            TrendingMovies(
+              trending: trendingmovies,
+            ),
+            TopRatedMovies(
+              toprated: topratedmovies,
+            ),
+          ],
+        ));
+  }
+}
